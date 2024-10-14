bbef991baf97de511e0d8753102d2364aa41a4ac
Niall Cardin <NiallC@gmail.com>
2024-10-14 22:00:08 +0100

    Very simple JSON parsin code added, however FileAccess seems to return empty data.

--- LoadCardData.gd
+++ LoadCardData.gd
@@ -1,18 +1,26 @@
 extends Node2D
 
 # Setup variables needed to read and parse the card data JSON file
-const cardsJsonFilepath = "res://Data/cards.json"
-var cardsJsonFile = FileAccess.open(cardsJsonFilepath, FileAccess.READ)
+const CARDS_JSON_FILEPATH = "res://Data/cards.json"
 var cardsJsonString = ""
 var cardsJson = JSON.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	cardsJsonFile.store_string(cardsJsonString)
-	var error = cardsJsonString.parse(cardsJson.error())
+	print("In card.tscn _ready...")
+	if FileAccess.file_exists(CARDS_JSON_FILEPATH):
+		var cardsJsonFile = FileAccess.open(CARDS_JSON_FILEPATH, FileAccess.READ)
+		cardsJsonFile.store_string(cardsJsonString)
+		var error = cardsJson.parse(cardsJsonString)
+		if error:
+			print("Some kind of error in parse the cards JSON...")
+			print("error = ", error)
+		print("cardsJsonString = ", cardsJsonString)
+	else:
+		print("Could not find file", CARDS_JSON_FILEPATH)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
